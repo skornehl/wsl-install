0854b3d635246477bf9b4d936d2ba35ca98738a9
Sebastian Kornehl <skornehl@users.noreply.github.com>
2022-04-06 09:14:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e -x
+set -x
 
 sudo apt-get update
 sudo apt-get upgrade -y 
@@ -19,12 +19,16 @@ sudo apt-get install -y \
 #### Docker
 ###### See https://dev.to/felipecrs/simply-run-docker-on-wsl2-3o8
 sudo apt-get remove docker docker-engine docker.io containerd runc
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor  --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+
 echo \
-  sudo apt-add-repository -y "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" 
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo groupadd docker
+sudo (groupadd docker || true)
 sudo usermod -aG docker skornehl
 
 #### Docker Compose
